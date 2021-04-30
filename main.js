582d70a7de3b40010e7f4d14966ac0b8da2e0d90
David Dygert <david.dygert@gmail.com>
2021-04-30 15:42:50 -0500

    Changed to port 80

--- main.js
+++ main.js
@@ -14,6 +14,6 @@ var server = http.createServer(function (request, response) {
   }
 });
 
-// listen on localhost:8000
+// listen on localhost:80
 server.listen(80);
 console.log("Server listening at http://127.0.0.1:80/");
